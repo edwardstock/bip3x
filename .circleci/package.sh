02dfbb845378ca55c23be287f13b59e2e04291d9
Eduard Maximovich <edward.vstock@gmail.com>
2019-06-21 15:17:30 +0300

    packaging

--- .circleci/package.sh
+++ .circleci/package.sh
@@ -4,8 +4,8 @@ mkdir -p _build_package && cd _build_package
 
 
 cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=$(pwd)/_install
-cmake --build . --target minter_tx_merge -- -j4
-cmake -DCOMPONENT=minter-sdk -P cmake_install.cmake
+cmake --build . -- -j4
+cmake -DCOMPONENT=main -P cmake_install.cmake
 
 function to_lower() {
   local outRes=$(echo ${1} | awk '{ for ( i=1; i <= NF; i++) {   sub(".", substr(tolower($i),1,1) , $i)  } print }')
@@ -19,13 +19,29 @@ then
   exit 1
 fi
 
+VERS=`git rev-parse --short HEAD`
+if [ -f "version" ]
+then
+    VERS=`cat version | tr -d "\n"`
+fi
+
+if [ -f "../version" ]
+then
+    VERS=`cat ../version | tr -d "\n"`
+fi
+
+if [ "${1}" != "" ]
+then
+    VERS=${1}
+fi
+
 arch=$(uname -m)
 sysname=$(uname)
 sysname=$(to_lower ${sysname})
 gvers=$(git tag)
 ghash=$(git rev-parse --short=8 HEAD)
 fname_sufix="${gvers}_${ghash}_${sysname}_${arch}"
-fname="libminter_tx_${fname_sufix}.tar.gz"
+fname="libbip39_${fname_sufix}.tar.gz"
 
 cd $(pwd)/_install
 
@@ -33,6 +49,7 @@ tar -zcvf ${fname} .
 
 mv ${fname} ../../output
 cd ../../output
+ghr ${VERS} .
 
 
 
